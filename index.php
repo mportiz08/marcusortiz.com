b7342428a266f4ffcbaf8cf619737bc23ec06c85
Marcus Ortiz <mportiz08@gmail.com>
2010-07-09 16:46:41 -0700

    worked on header layout

--- index.php
+++ index.php
@@ -9,8 +9,20 @@
 <div id="background">
   <div id="container">
     <div id="top-bar"></div>
-    <div id="main-nav"></div>
-    <div id="content"></div>
+    <div id="wrapper">
+      <div id="main-nav">
+        <div id="logo"><a href="http://marcusortiz.com"><img src="img/logo.png" alt="marcusortiz.com" /></a></div>
+        <div id="nav">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
+      <div id="content"></div>
+    </div>
     <div id="footer"></div>
   </div>
 </div>
