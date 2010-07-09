f9026baac39b57e297e32be474485ca744bfac2a
Marcus Ortiz <mportiz08@gmail.com>
2010-07-08 22:03:46 -0700

    starting work on layout

--- index.php
+++ index.php
@@ -3,8 +3,16 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
   <title>coming soon</title>
+  <link href="css/main.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
-  <p>coming soon</p>
+<div id="background">
+  <div id="container">
+    <div id="top-bar"></div>
+    <div id="main-nav"></div>
+    <div id="content"></div>
+    <div id="footer"></div>
+  </div>
+</div>
 </body>
 </html>
