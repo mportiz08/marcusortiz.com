a358b6b8754255baedf28392be5f01b7e1fce21c
Marcus Ortiz <mportiz08@gmail.com>
2010-07-09 20:16:24 -0700

    added links in top bar

--- index.php
+++ index.php
@@ -8,7 +8,20 @@
 <body>
 <div id="background">
   <div id="container">
-    <div id="top-bar"></div>
+    <div id="top-bar">
+      <div id="links">
+        <ul>
+          <li><a href="http://facebook.com/marcusortiz"><img src="img/icon_facebook.png" /></a></li>
+          <li><a href="http://twitter.com/mportiz08"><img src="img/icon_twitter.png" /></a></li>
+          <li><a href="http://last.fm/user/chipitople"><img src="img/icon_lastfm.png" /></a></li>
+          <li><a href="http://flickr.com/mportiz08"><img src="img/icon_flickr.png" /></a></li>
+          <li><a href="http://delicious.com/chipitople"><img src="img/icon_delicious.png" /></a></li>
+          <li><a href="http://digg.com/users/chipitople"><img src="img/icon_digg.png" /></a></li>
+          <li><a href="http://vimeo.com/mportiz08"><img src="img/icon_vimeo.png" /></a></li>
+          <li><a href="http://youtube.com/mportiz08"><img src="img/icon_youtube.png" /></a></li>
+        </ul>
+      </div>
+    </div>
     <div id="wrapper">
       <div id="main-nav">
         <div id="logo"><a href="http://marcusortiz.com"><img src="img/logo.png" alt="marcusortiz.com" /></a></div>
