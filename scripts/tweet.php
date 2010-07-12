ea565ae46bc08b0ed77c33d25fdf783276b08e8f
Marcus Ortiz <mportiz08@gmail.com>
2010-07-11 21:07:52 -0700

    finished basic tweet script

--- scripts/tweet.php
+++ scripts/tweet.php
@@ -1,17 +1,25 @@
 <?php
 function getTweet()
 {
-  $url = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=mportiz08&count=1"
+  // check rate limit
+  $url = "http://api.twitter.com/1/account/rate_limit_status.json";
+  $json = file_get_contents($url);
+  $response = json_decode($json, true);
+  $limit_exceeded = $response["remaining_hits"] == 0;
+  
+  // request tweet
+  $url = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=mportiz08&count=1";
   $json = file_get_contents($url);
   $response = json_decode($json, true);
 
-  if(is_null($response))
+  if(is_null($response) || $limit_exceeded)
   {
-    echo "Not available at this time, try again later."
+    echo "Not available at this time, try again later.";
   }
   else
   {
-    // TODO
+    //var_dump($response);
+    echo $response[0]["text"];
   }
 }
 ?>
