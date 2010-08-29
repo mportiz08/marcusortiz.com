a2d076e8ef5fdee6989c82b6cee66c46dde0e749
Marcus Ortiz <mportiz08@gmail.com>
2010-08-29 11:28:05 -0700

    added ugly, semi-working hack for ie, removed javascript stuff

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ require 'models/last_fm'
 set :haml, { :format => :html5 }
 
 get '/' do
+  @ie_hack = IEHack.get
   @song = LastFM::User.new("chipitople").put_last_song
   haml :index
 end
@@ -15,4 +16,18 @@ end
 get '/application.css' do
   content_type 'text/css', :charset => 'utf-8'
   sass :style
+end
+
+module IEHack
+  def self.get
+    "<!--[if IE]>
+       <style type=\"text/css\">
+         #header {
+           background:#000000;
+           filter:alpha(opacity=50)
+           zoom: 1;
+         } 
+       </style>
+     <![endif]-->"
+  end
 end
