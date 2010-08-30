adcd1c8daed8335f7ed0f422b36570de895a6fde
Marcus Ortiz <mportiz08@gmail.com>
2010-08-30 00:52:24 -0700

    added code snippet

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 require 'sass'
+require 'open-uri'
 
 require 'models/last_fm'
 
@@ -10,12 +11,17 @@ set :haml, { :format => :html5 }
 get '/' do
   @ie_hack = IEHack.get
   @song = LastFM::User.new("chipitople").put_last_song
+  @snippet = get_snippet
   haml :index
 end
 
 get '/application.css' do
   content_type 'text/css', :charset => 'utf-8'
   sass :style
+end
+
+def get_snippet
+  open("snippet.rb").read
 end
 
 module IEHack
---
+++ snippet.rb
@@ -0,0 +1,5 @@
+
+def latest_project
+  @url = "http://marcusortiz.com/project/latest"
+  redirect_to @url
+end
